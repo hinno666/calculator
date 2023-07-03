308c5c762879fb393a40b1fdea843efc142cc049
Andrey <luffytaroyo@gmail.com>
2023-07-03 23:45:55 +0300

    added functional for my calculator

--- js/main.js
+++ js/main.js
@@ -1,33 +1,72 @@
-const result = document.querySelector(".calculator__display");
-const calculatorNumbers = document.querySelectorAll(".calculator__key");
-const calculatorOperators = document.querySelectorAll(".operator");
-const clearBtn = document.querySelector(".clear");
-const equalBtn = document.querySelector(".equal");
+let a = ''; // первое число
+let b = ''; // второе число
+let sign = ''; // экран операции
+let finish = false;
 
+const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
 
+const action = ['-', '+', '*', '/'];
+
+const result = document.querySelector('.calculator__display');
+const buttons = document.querySelectorAll('.calculator__key');
 
 function clear() {
-  result.value = "";
+    a = '';
+    b = '';
+    sign = '';
+    finish = false;
+    result.value = 0;
 }
 
-calculatorNumbers.forEach((button) => {
-  button.addEventListener("click", () => {
-        result.value += button.textContent;
-        const resultArr = result.value.split(/[\+\-\*\/]/);
-        console.log(resultArr);
-  });
-});
-
-clearBtn.addEventListener("click", clear);
+buttons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        if(button.classList.contains('clear')) {
+            clear();
+        }
 
 
+        const key = e.target.textContent;
 
-calculatorOperators.forEach((button) => {
-    button.addEventListener('click', () => {
-        
+
+        if(digit.includes(key)) {
+            if(b === '' & sign === ''){
+                a += key;
+                result.value = a;
+            }else if(a !== '' && b !== '' & finish)  {
+                b = key;
+                finish = false;
+                result.value = b;
+            }else {
+                b += key;
+                result.value = b;
+            }
+            return
+        }
+
+        if(action.includes(key)) {
+            sign = key;
+            result.value = sign;
+            return
+        }
+
+
+        if(key === '=') {
+            switch(sign) {
+                case "+":
+                a = Number(a) + Number(b);
+                break;
+                case '-':
+                a = a -b;
+                break;
+                case '*':
+                a = a * b;
+                break;
+                case '/':
+                a = a / b;
+                break;
+            }
+            finish = true;
+            result.value = a;
+        }
     })
-});
-
-equalBtn.addEventListener("click", () => {
-    
-});
+})
